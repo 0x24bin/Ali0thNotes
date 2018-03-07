2007033dbd581a071ba0b4dbbe5309963510d80a
martin2877 <martin2877@outlook.com>
2018-03-07 17:21:03 +0800

    update

--- Protocols Security/script/suricata rules update management/srum.py
+++ Protocols Security/script/suricata rules update management/srum.py
@@ -7,6 +7,10 @@ SRUM is short for Suricata Rules Update Management , a tool to compare and updat
 Author : ali0th
 Date   : 18/3/7
 Email  : martin2877 at foxmail.com
+
+Usage  : python srum.py [rules file] [keyword]
+example: python srum.py emerging-web_specific_apps.rules struts
+
 """
 
 import requests
@@ -15,8 +19,10 @@ import os
 import sys
 import time
 
-OFFICIAL_PATH = r'https://rules.emergingthreats.net/open/suricata-4.0/rules/'
-LOCAL_PATH = r"C:/Users/muhe/Desktop/links/tophant/suricata/nazgul-ids_prs2.1/rules/"
+OFFICIAL_PATH = r'https://rules.emergingthreats.net/open/suricata-4.0/rules/' # official rules addr
+LOCAL_PATH = r"C:/Users/muhe/Desktop/links/tophant/suricata/nazgul-ids_prs2.1/rules/" # local rules path
+TEMP_PATH = r"C:/Users/muhe/Desktop/links/wirte/Ali0thNotes/Protocols Security/script/suricata rules update management/" # temp addr for file cache
+
 
 def get_text(path, method):
     try:
@@ -26,15 +32,30 @@ def get_text(path, method):
         elif method == 2:
             r = requests.get(path)
             return r.text, True
+        elif method == 3: # read temp file first
+            temp_file_name = os.path.basename(path)
+            temp_file = TEMP_PATH + temp_file_name
+            if os.path.exists(temp_file):
+                return get_text(temp_file, 1)
+            else:
+                file_text, is_text = get_text(path, 2)
+                with open(temp_file, "w") as f:
+                    f.write(file_text)
+                return file_text, True
+
     except Exception as e:
         return e, False
+
+
+def get_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 
 def match_file(content):
     pattern = re.compile(r'<a href="[^?^/].*?">(.*?)</a>')
     match = pattern.findall(str(content))
     if match:
-        print("Receive Offical Rules Success {0}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        print("Receive Offical Rules Success {0}".format(get_time()))
         return match,True
     else:
         return [],False
@@ -60,20 +81,28 @@ def find_regex(content, regex):
 def compare_files(file1, file2, regex=""):
 
     def compare_sid(content1, content2):
+        print("="*20)
+        print("strat compare_sid  {0}".format(get_time()))
         re = r'sid:([0-9]*?);'
         sid_list1 = find_regex(content1, re)
         sid_list2 = find_regex(content2, re)
+        print("="*20)
+        print("strat compare sid_list1 to 2  {0}".format(get_time()))
         for sid1 in sid_list1:
             if sid1 in sid_list2:
                 re = r'.*sid:{0};.*'.format(sid1)
                 print("="*20)
                 print("local : {0}\n".format(find_regex(content1, re)[0]))
                 print("official : {0}\n".format(find_regex(content2, re)[0]))
+        print("="*20)
+        print("strat compare sid_list2 to 1  {0}".format(get_time()))
         for sid2 in sid_list2:
             if sid2 not in sid_list1:
                 re = r'.*sid:{0};.*'.format(sid2)
                 print("="*20)
-                print("add : ", find_regex(content2, re)[0])
+                print("add : {0}\n".format(find_regex(content2, re)[0]))
+        print("="*20)
+        print("process end {0}".format(get_time()))
 
     if regex:
         re = r'.*{0}.*'.format(regex)
@@ -87,33 +116,43 @@ def compare_files(file1, file2, regex=""):
 def start(command = "all", command2 = ""):
     url = OFFICIAL_PATH
     official_files, is_match = match_file(get_text(url, 2))
+    print("="*20)
+    print("start search files {0}".format(get_time()))
+    all_files = get_files(LOCAL_PATH, ["rules"])
+    print("end search files {0}".format(get_time()))    
     if is_match:
-        for file_name in official_files:
-            all_files = get_files(LOCAL_PATH, ["rules"])
-            if command == "all":
+        if command == "all":
+            for file_name in official_files:
                 if LOCAL_PATH + file_name in all_files:
                     print("="*20)
                     print("Now comparing {0}".format(file_name))
                     # 读取文件内容
-                    file1, is_text = get_text(LOCAL_PATH + file_name, 1)
-                    file2, is_text = get_text(OFFICIAL_PATH + file_name,2)
-                    compare_files(file1, file2)
+                    file1, is_text1 = get_text(LOCAL_PATH + file_name, 1)
+                    file2, is_text2 = get_text(OFFICIAL_PATH + file_name,3)
+                    if is_text1 and is_text2:
+                        compare_files(file1, file2)
+                    else:
+                        print("empty file")
                 else:
                     print("="*20)
                     print("{0} is not in local path".format(file_name))
-            elif LOCAL_PATH + command in all_files:
-                print("="*20)
-                print("Now comparing {0}".format(command))
-                # 读取文件内容
-                file1, is_text = get_text(LOCAL_PATH + command, 1)
-                file2, is_text = get_text(OFFICIAL_PATH + command,2)
-                compare_files(file1, file2, command2)
-                break
+        elif LOCAL_PATH + command in all_files:
+            print("="*20)
+            print("Now comparing {0}".format(command))
+            # 读取文件内容
+            print("="*20)
+            print("Now read file 1 {0} {1}".format(command, get_time()))
+            file1, is_text1 = get_text(LOCAL_PATH + command, 1)
+            print("="*20)
+            print("Now read file 2 {0} {1}".format(command, get_time()))
+            file2, is_text2 = get_text(OFFICIAL_PATH + command,3)
+            if is_text1 and is_text2:
+                compare_files(file1, file2)
             else:
-                print("="*20)
-                print("{0} is not in local path".format(command))
-                break
-
+                print("empty file")
+        else:
+            print("="*20)
+            print("{0} is not in local path".format(command))
     else:
         print("Not match!")
 
@@ -126,8 +165,8 @@ if __name__ == '__main__':
     else:
         command = "all"
         command2 = ""
-    ## 测试语句
-    command = "emerging-web_specific_apps.rules"
-    command2 = r"struts"
+
+    command = "emerging-web_specific_apps.rules"    ## 测试语句
+    command2 = r"struts"    ## 测试语句
 
     start(command, command2)
---
+++ Protocols Security/script/suricata rules update management/test.py
@@ -0,0 +1,3 @@
+import os
+
+print(os.path.basename('https://rules.emergingthreats.net/open/suricata-4.0/rules/emerging-web_specific_apps.rules'))
